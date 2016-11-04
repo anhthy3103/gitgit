24da05841bcee6575e632b024299c85389cf2dbe
Thy <anhthy3103@gmail.com>
2016-11-04 13:22:08 +0700

    git git git

---
+++ gitDemo/src/gitDemo/git1.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package gitDemo;
+
+import javax.swing.JFrame;
+
+/**
+ * @author User
+ *
+ */
+public class git1 extends JFrame {
+	public git1(){
+		setSize(300,300);
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		git1 f = new git1();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+
+}
